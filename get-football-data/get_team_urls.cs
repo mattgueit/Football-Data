51ca3c79cb729f5843bbbccb2d250a33036f971e
Matt Gueit <mattgueit@gmail.com>
2023-09-13 21:35:20 +1000

    Add Queue Output Binding for get_team_urls using test data.

--- get-football-data/get_team_urls.cs
+++ get-football-data/get_team_urls.cs
@@ -1,6 +1,6 @@
-using System;
+using System.Collections.Generic;
+using System.Text.Json;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace get_football_data
@@ -8,9 +8,48 @@ namespace get_football_data
     public class get_team_urls
     {
         [FunctionName("get_team_urls")]
-        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        [return: Queue("team-urls")]
+        public string[] Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation("Fetching team urls");
+
+            var teamUrls = new List<string>();
+
+            string[] testTeamNames = {
+                "Arsenal",
+                "Aston Villa",
+                "Bournemouth",
+                "Brentford",
+                "Brighton",
+                "Burnley",
+                "Chelsea",
+                "Crystal Palace",
+                "Everton",
+                "Fulham",
+                "Liverpool",
+                "Luton Town",
+                "Manchester City",
+                "Manchester United",
+                "Newcastle",
+                "Nottingham Forest",
+                "Sheffield United",
+                "Tottenham",
+                "West Ham",
+                "Wolves"
+            };
+
+            foreach (var teamName in testTeamNames)
+            {
+                var teamObject = new
+                {
+                    teamName,
+                    teamUrl = $"www.transfermarkt.com/team/{teamName}"
+                };
+
+                teamUrls.Add(JsonSerializer.Serialize(teamObject, new JsonSerializerOptions { WriteIndented = false }));
+            }
+
+            return teamUrls.ToArray();
         }
     }
 }
