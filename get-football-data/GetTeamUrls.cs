2e5d8a7494607868bc6add5463d980a7387cfed4
Matt Gueit <mattgueit@gmail.com>
2023-09-15 16:44:08 +1000

    Actually scrape TransferMarkt for team urls.

--- get-football-data/GetTeamUrls.cs
+++ get-football-data/GetTeamUrls.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
+using get_football_data.Models;
+using HtmlAgilityPack;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -8,49 +12,88 @@ namespace get_football_data
     [StorageAccount("AzureWebJobsStorage")]
     public class GetTeamUrls
     {
+        const string baseUrl = "https://www.transfermarkt.com";
+        const string premierLeagueTeamsPath = "/premier-league/startseite/wettbewerb/GB1";
+        const string teamsTableXPath = "//*[@id=\"yw1\"]/table/tbody";
+        const string teamRowsXPath = "//*[@class=\"hauptlink no-border-links\"]";
+
         [FunctionName("get_team_urls")]
         [return: Queue("team-urls")]
         public string[] Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation("Fetching team urls");
+            log.LogInformation("Fetching team urls.");
 
-            var teamUrls = new List<string>();
-
-            string[] testTeamNames = {
-                "Arsenal",
-                "Aston Villa",
-                "Bournemouth",
-                "Brentford",
-                "Brighton",
-                "Burnley",
-                "Chelsea",
-                "Crystal Palace",
-                "Everton",
-                "Fulham",
-                "Liverpool",
-                "Luton Town",
-                "Manchester City",
-                "Manchester United",
-                "Newcastle",
-                "Nottingham Forest",
-                "Sheffield United",
-                "Tottenham",
-                "West Ham",
-                "Wolves"
-            };
-
-            foreach (var teamName in testTeamNames)
+            try
             {
-                var teamObject = new
-                {
-                    teamName,
-                    teamUrl = $"www.transfermarkt.com/team/{teamName}"
-                };
+                var teamUrls = new List<string>();
 
-                teamUrls.Add(JsonSerializer.Serialize(teamObject, new JsonSerializerOptions { WriteIndented = false }));
+                var htmlDocument = this.LoadTeamsPage();
+
+                var teamsTable = this.SelectTeamsTable(htmlDocument);
+
+                var teamRows = this.SelectTeamRows(teamsTable);
+
+                foreach (var teamRow in teamRows)
+                {
+                    var attributes = teamRow.FirstChild.Attributes;
+
+                    var title = attributes["title"].Value;
+                    var href = attributes["href"].Value;
+
+                    var teamUrl = new TeamUrl
+                    {
+                        TeamName = title,
+                        Url = $"{baseUrl}{href}"
+                    };
+
+                    teamUrls.Add(JsonSerializer.Serialize(teamUrl));
+                }
+
+                return teamUrls.ToArray();
+            }
+            catch (Exception ex) 
+            {
+                log.LogError(ex, $"Error getting team urls: {ex.Message}");
+                return Array.Empty<string>();
+            }
+        }
+
+        private HtmlDocument LoadTeamsPage()
+        {
+            var web = new HtmlWeb();
+
+            var doc = web.Load($"{baseUrl}{premierLeagueTeamsPath}");
+
+            if (doc == null)
+            {
+                throw new Exception("Teams page not found.");
             }
 
-            return teamUrls.ToArray();
+            return doc;
+        }
+
+        private HtmlNode SelectTeamsTable(HtmlDocument htmlDocument)
+        {
+            var teamsTable = htmlDocument.DocumentNode.SelectNodes(teamsTableXPath).First();
+
+            if (teamsTable == null)
+            {
+                throw new Exception("Could not find the premier league teams table.");
+            }
+
+            return teamsTable;
+        }
+
+        private HtmlNodeCollection SelectTeamRows(HtmlNode teamsTable)
+        {
+            var teamRows = teamsTable.SelectNodes(teamRowsXPath);
+
+            if (teamRows == null)
+            {
+                throw new Exception("Could not find the premier league team rows.");
+            }
+
+            return teamRows;
         }
     }
 }
---
+++ get-football-data/Models/TeamUrl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace get_football_data.Models
+{
+    public class TeamUrl
+    {
+        public string TeamName { get; set; }
+        public string Url { get; set; }
+    }
+}
