6520e55537b8c8961587a1c18028e09f778ada70
Matt Gueit <mattgueit@gmail.com>
2023-10-16 14:30:18 +1100

    Add player scraping per team.

---
+++ get-football-data/ErrorHandling/Throw.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace get_football_data.ErrorHandling
+{
+    public static class Throw
+    {
+        public static void IfNull(object? obj, string objectName)
+        {
+            if (obj == null)
+            {
+                throw new Exception($"Data not found: {objectName}.");
+            }
+        }
+
+        public static void IfNullOrEmpty(IEnumerable<object>? obj, string collectionName)
+        {
+            if (obj == null || !obj.Any())
+            {
+                throw new Exception($"Data not found: {collectionName}");
+            }
+        }
+    }
+}
--- get-football-data/GetPlayerDetails.cs
+++ get-football-data/GetPlayerDetails.cs
@@ -1,15 +1,286 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using get_football_data.ErrorHandling;
+using get_football_data.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using HtmlAgilityPack;
 
 namespace get_football_data
 {
     [StorageAccount("AzureWebJobsStorage")]
     public class GetPlayerDetails
     {
+        const string baseUrl = "https://www.transfermarkt.com";
+
         [FunctionName(nameof(GetPlayerDetails))]
-        public void Run([QueueTrigger("team-urls")] string teamUrl, ILogger log)
+        [return: Queue("player-data")]
+        public string[] Run([QueueTrigger("team-urls")] string teamUrlString, ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {teamUrl}");
+            var teamUrl = JsonSerializer.Deserialize<TeamUrl>(teamUrlString);
+
+            log.LogInformation($"Fetching player data for {teamUrl.TeamName}");
+
+            var htmlDocument = LoadPage(teamUrl.Url);
+
+            var playerTable = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"yw1\"]/table/tbody");
+            Throw.IfNull(playerTable, "player table");
+
+            var playerRows = playerTable.SelectNodes("./*[@class='even' or @class='odd']");
+            Throw.IfNullOrEmpty(playerRows, "player rows");
+
+            var playerData = new List<string>();
+
+            foreach (var playerRow in playerRows)
+            {
+                var playerColumns = playerRow.ChildNodes
+                    .Where(x => x.Name == "td")
+                    .ToList();
+
+                ValidatePlayerColumns(playerColumns);
+
+                var playerDetails = new PlayerDetails()
+                {
+                    TeamName = "Arsenal" // MG TODO: replace with actual team name. This can be grabbed from the message in the queue.
+                };
+
+                for (int i = 0; i < playerColumns.Count; i++)
+                {
+                    var playerColumn = playerColumns[i];
+
+                    switch (i)
+                    {
+                        case 0:
+                            GetColumn0PlayerData(playerDetails, playerColumn);
+                            break;
+                        case 1:
+                            GetColumn1PlayerData(playerDetails, playerColumn);
+                            break;
+                        case 2:
+                            // No data in this column.
+                            break;
+                        case 3:
+                            GetColumn3PlayerData(playerDetails, playerColumn);
+                            break;
+                        case 4:
+                            GetColumn4PlayerData(playerDetails, playerColumn);
+                            break;
+                        case 5:
+                            GetColumn5PlayerData(playerDetails, playerColumn);
+                            break;
+                        default:
+                            throw new Exception($"Unexpected player details column [{i}].");
+                    }
+                }
+
+                playerData.Add(JsonSerializer.Serialize(playerDetails));
+            }
+
+            return playerData.ToArray();
+        }
+
+        private static HtmlDocument LoadPage(string url)
+        {
+            var web = new HtmlWeb();
+
+            var doc = web.Load(url);
+
+            Throw.IfNull(doc, "team page");
+
+            return doc;
+        }
+
+        private static void GetColumn0PlayerData(PlayerDetails playerDetails, HtmlNode? playerColumn)
+        {
+            Throw.IfNull(playerColumn, "player column 0");
+
+            var playerPosition = playerColumn.Attributes["title"];
+            Throw.IfNull(playerPosition, "player position");
+
+            var shirtNumberDiv = playerColumn.SelectSingleNode("div");
+            Throw.IfNull(shirtNumberDiv, "player shirt number");
+
+            if (!int.TryParse(shirtNumberDiv.InnerText, out var shirtNumber))
+            {
+                throw new Exception($"Shirt number is not a valid integer: \"{shirtNumberDiv}\"");
+            }
+
+            playerDetails.Position = playerPosition.Value.ToUpper();
+            playerDetails.ShirtNumber = shirtNumber;
+        }
+
+        private static void GetColumn1PlayerData(PlayerDetails playerDetails, HtmlNode? playerColumn)
+        {
+            Throw.IfNull(playerColumn, "player column 1");
+
+            var inLineTableRows = playerColumn
+                .SelectNodes("./table/tbody/tr")
+                .ToList();
+
+            Throw.IfNullOrEmpty(inLineTableRows, "in-line table containing player image");
+
+            var playerImageUrl = inLineTableRows[0]
+                .SelectSingleNode("./td[1]/img")
+                .Attributes["data-src"]
+                .Value;
+
+            var anchorTag = inLineTableRows[0].SelectSingleNode("./td[2]/a");
+
+            var playerPagePath = anchorTag
+                .Attributes["href"]
+                .Value;
+
+            var playerName = anchorTag.InnerText;
+
+            var exactPosition = inLineTableRows[1]
+                .SelectSingleNode("./td")
+                .InnerText;
+
+            playerDetails.PlayerImageUrl = playerImageUrl;
+            playerDetails.PlayerPageUrl = $"{baseUrl}{playerPagePath}";
+            playerDetails.PlayerName = playerName;
+            playerDetails.ExactPosition = exactPosition;
+            playerDetails.Id = int.Parse(Path.GetFileName(playerPagePath));
+        }
+
+        private static void GetColumn3PlayerData(PlayerDetails playerDetails, HtmlNode? playerColumn)
+        {
+            Throw.IfNull(playerColumn, "player column 3");
+
+            playerDetails.DateOfBirth = GetDateOfBirth(playerColumn.InnerText);
+        }
+
+        private static void GetColumn4PlayerData(PlayerDetails playerDetails, HtmlNode? playerColumn)
+        {
+            Throw.IfNull(playerColumn, "player column 4");
+
+            var nationalityFlag = playerColumn.SelectSingleNode("./img");
+
+            Throw.IfNull(nationalityFlag, "player nationality flag");
+
+            var nationality = nationalityFlag
+                .Attributes["title"]
+                .Value;
+
+            var nationalityFlagUrl = nationalityFlag
+                .Attributes["src"]
+                .Value;
+
+            Throw.IfNull(nationality, "player nationality");
+            Throw.IfNull(nationalityFlagUrl, "player flag url");
+
+            playerDetails.Nationality = nationality;
+            playerDetails.NationalityFlagUrl = nationalityFlagUrl;
+        }
+
+        private static void GetColumn5PlayerData(PlayerDetails playerDetails, HtmlNode? playerColumn)
+        {
+            Throw.IfNull(playerColumn, "player column 5");
+
+            var marketValueAnchor = playerColumn.SelectSingleNode("./a");
+
+            Throw.IfNull(marketValueAnchor, "player market value");
+
+            var marketValue = GetMarketValue(marketValueAnchor.InnerText);
+
+            playerDetails.MarketValue = marketValue;
+        }
+
+        /// <summary>
+        /// Converts a date text to DateTime.
+        /// Expected format: May 14, 1998 (25)
+        /// i.e {Month} {Day}, {Year} ({Age})
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        private static DateTime GetDateOfBirth(string dateText)
+        {
+            const string pattern = @"(\w+) (\d+), (\d+)";
+
+            var match = Regex.Match(dateText, pattern);
+
+            if (match.Success)
+            {
+                var monthText = match.Groups[1].Value;
+
+                if (!int.TryParse(match.Groups[2].Value, out var day))
+                {
+                    throw new Exception($"Failed to parse day {match.Groups[2].Value} to its numeric form.");
+                }
+
+                if (!int.TryParse(match.Groups[3].Value, out var year))
+                {
+                    throw new Exception($"Failed to parse year {match.Groups[3].Value} to its numeric form.");
+                }
+
+                if (!DateTime.TryParseExact(monthText, "MMM", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out var dateTimeResult))
+                {
+                    throw new Exception($"Failed to parse month {monthText} to its numeric form. Expecting a 3 character format (e.g. Jan, Mar, Aug ...)");
+                }
+
+                var numericMonth = dateTimeResult.Month;
+
+                return new DateTime(year, numericMonth, day);
+            }
+            else
+            {
+                throw new Exception($"Player date of birth was in an unexpected format: {dateText}");
+            }
+        }
+
+        /// <summary>
+        /// Converts a market value text to an integer value.
+        /// Expected formats: "€50.00m", "€4.50m", "€500k".
+        /// Also handling future edge case: "€1.00b".
+        /// </summary>
+        /// <param name="marketValueText"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        private static int GetMarketValue(string marketValueText)
+        {
+            // should either m (million) or k (thousand). Handling b (billion) in case the market gets even worse...
+            var multiplier = marketValueText[marketValueText.Length - 1];
+
+            var numericMarketValue = marketValueText.Substring(1, marketValueText.Length - 2);
+
+            int factor;
+
+            // in order of most common to least common.
+            switch (multiplier)
+            {
+                case 'm':
+                    factor = 1000000;
+                    break;
+                case 'k':
+                    factor = 1000;
+                    break;
+                case 'b':
+                    factor = 1000000000;
+                    break;
+                default:
+                    throw new Exception($"Unexpected market value multiplier: {multiplier}");
+            }
+
+            var floatValue = float.Parse(numericMarketValue);
+            var result = (int)(floatValue * factor);
+
+            return result;
+        }
+
+
+        private static void ValidatePlayerColumns(List<HtmlNode?> playerColumns)
+        {
+            Throw.IfNullOrEmpty(playerColumns, "player columns");
+
+            if (playerColumns.Count != 6)
+                throw new Exception($"Expected 6 player columns. Instead found {playerColumns.Count}");
+
+            if (playerColumns.Any(x => x == null))
+                throw new Exception($"A null player column was found.");
         }
     }
 }
